b4eb883b20703ab1ec03dbdef1936983f7c2bf4f
rturovtsev <rturovtsev@yandex.ru>
2025-01-28 18:42:00 +0300

    update metateg

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/bogem/id3v2/v2"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"regexp"
+	"sort"
 	"strconv"
 )
 
 type AudioFile struct {
-	Path  string
-	Index int
+	Path    string
+	NewName string
+	Index   int
 }
 
 type ByIndex []AudioFile
@@ -25,13 +29,23 @@ func main() {
 	inputDir := flag.String("input", "", "Path to the directory with MP3 files")
 	author := flag.String("author", "", "Author of the book")
 	title := flag.String("title", "", "Title of the book")
-
 	flag.Parse()
 
 	if *inputDir == "" {
 		log.Fatalf("imput directory is required")
 	}
 
+	mp3Files, err := getMP3Files(*inputDir)
+	if err != nil {
+		log.Fatalf("error getting MP3 files: %v", err)
+	}
+
+	updateMetaTags(mp3Files, author, title)
+
+	err = mergeFilesToM4B(mp3Files, *author, *title)
+	if err != nil {
+		log.Fatalf("error merging files: %v", err)
+	}
 }
 
 func getMP3Files(inputDir string) ([]AudioFile, error) {
@@ -41,15 +55,25 @@ func getMP3Files(inputDir string) ([]AudioFile, error) {
 	}
 
 	var audioFiles []AudioFile
+	var newName string
+	var index int
+
+	fileCount := len(files)
 	for _, file := range files {
 		if filepath.Ext(file.Name()) == ".mp3" {
-			newName, index, err := parseIndex(file.Name(), len(files))
+			oldName := file.Name()
+			oldPath := filepath.Join(inputDir, oldName)
+			newPath := filepath.Join(inputDir, newName)
+			newName, index, err = parseIndex(oldName, fileCount)
 			if err != nil {
 				return nil, err
 			}
-			audioFiles = append(audioFiles, AudioFile{Path: filepath.Join(inputDir, newName), Index: index})
+			renameFiles(oldPath, newPath)
+			audioFiles = append(audioFiles, AudioFile{Path: newPath, NewName: newName, Index: index})
 		}
 	}
+	sort.Sort(ByIndex(audioFiles))
+
 	return audioFiles, nil
 }
 
@@ -76,4 +100,64 @@ func parseIndex(filename string, countAllFile int) (newName string, index int, e
 	}
 
 	return newName, index, nil
+}
+
+func renameFiles(oldPath, newPath string) {
+	err := os.Rename(oldPath, newPath)
+	if err != nil {
+		log.Fatalf("error renaming file: %v", err)
+	}
+}
+
+func updateMetaTags(files []AudioFile, author, title *string) {
+	totalFiles := len(files)
+	for _, file := range files {
+		tag, err := id3v2.Open(file.Path, id3v2.Options{Parse: true})
+		if err != nil {
+			log.Fatalf("error opening tag for file %s: %v", file.Path, err)
+		}
+		defer tag.Close()
+
+		// Очистка существующего исполнителя альбома
+		tag.DeleteFrames("TPE2")
+
+		// Установка номера трека
+		trackNum := fmt.Sprintf("%s/%d", file.NewName, totalFiles)
+		tag.AddTextFrame("TRCK", tag.DefaultEncoding(), trackNum)
+
+		// Установка номера диска, если требуется
+		tag.AddTextFrame("TPOS", tag.DefaultEncoding(), "1")
+
+		// Удаление комментариев
+		tag.DeleteFrames("COMM")
+
+		// Установка имени автора и названия альбома, если предоставлены
+		if author != nil && *author != "" {
+			tag.AddTextFrame("TPE1", tag.DefaultEncoding(), *author)
+		}
+
+		if title != nil && *title != "" {
+			tag.AddTextFrame("TALB", tag.DefaultEncoding(), *title)
+		}
+
+		// Сохранение изменений в файле
+		if err = tag.Save(); err != nil {
+			log.Fatalf("error saving tag for file %s: %v", file.Path, err)
+		}
+	}
+}
+
+func mergeFilesToM4B(files []AudioFile, author, title string) error {
+	var args []string
+	for _, file := range files {
+		args = append(args, "-i", file.Path)
+	}
+	outputFile := filepath.Join(filepath.Dir(files[0].Path), "output.m4b")
+
+	args = append(args, "-c:a", "aac", "-vn", "-ar", "44100", "-ab", "256k", "-f", "mp4", outputFile)
+
+	cmd := exec.Command("ffmpeg", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/rturovtsev/audiobook-processor
 
 go 1.23.0
+
+require github.com/bogem/id3v2/v2 v2.1.4
+
+require golang.org/x/text v0.3.8 // indirect
